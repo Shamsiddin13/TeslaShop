124291e3c9982de485b39f67d856464029175b31
Shamsiddin_Dev <119076053+Shamsiddin13@users.noreply.github.com>
2023-10-10 15:03:38 +0500

    add productId

--- TeslaShop.Domain/Models/Order.cs
+++ TeslaShop.Domain/Models/Order.cs
@@ -8,6 +8,9 @@ public class Order : Auditable
     public long CustomerId { get; set; }
     public User Customer { get; set; }
 
+    public long ProductId { get; set; }
+    public Product Product { get; set; }
+
     public OrderStatus OrderStatus { get; set; }
     public decimal TotalAmount { get; set; }
 }
